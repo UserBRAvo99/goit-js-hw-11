07eccd6cd0b21d2153d7ee5c8df505f32e0eb102
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-02-03 21:05:53 +0200

    fetch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { KEY, BASE_URL, fetchUsersEntered } from './js/fetch';
-import { handleInput } from './js/event';
 
 console.log(
   `${BASE_URL}${KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`
 );
 console.log(fetchUsersEntered());
-console.log(handleInput);
