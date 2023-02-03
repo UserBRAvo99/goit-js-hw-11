0ee73a21a96b2aee041cedb92e8f6eee1293fa89
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-02-03 21:10:51 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { KEY, BASE_URL, fetchUsersEntered } from './js/fetch';
 console.log(
   `${BASE_URL}${KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true`
 );
-console.log(fetchUsersEntered());
+console.log(fetchUsersEntered);
