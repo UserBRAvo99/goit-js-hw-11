ddf7dd8f0736b6d6d928b2b44c3ffa7d5feb93f6
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-02-05 23:44:48 +0200

    commit

--- src/js/fetch/index.js
+++ src/js/fetch/index.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+// import axios from 'axios';
 import scrollWatch from 'infinite-scroll/js/scroll-watch';
 import Notiflix from 'notiflix';
+
+// axios.defaults.baseURL = 'http://localhost:3000/contacts';
 
 const KEY = '33349465-f6e14a947762518aeccde2783';
 const BASE_URL = 'https://pixabay.com/api/?key=';
 const image = 'image_type=photo';
 const orientation = 'orientation=horizontal';
 const safesearch = 'safesearch=true';
-// const perPage = 40;
-// const page = 1;
 
 export async function fetchUsersEntered(name, page = 1, perPage = 40) {
   const response = await fetch(
@@ -17,6 +17,12 @@ export async function fetchUsersEntered(name, page = 1, perPage = 40) {
   const result = await response.json();
   return result;
 }
+
+// const KEY = '33349465-f6e14a947762518aeccde2783';
+// const BASE_URL = 'https://pixabay.com/api/?key=';
+// const image = 'image_type=photo';
+// const orientation = 'orientation=horizontal';
+// const safesearch = 'safesearch=true';
 
 // export async function fetchUsersEntered(name, page = 1, perPage = 40) {
 //   try {
